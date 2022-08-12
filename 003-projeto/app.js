0580bffef7f764ad56dcafbd1487dad927097f50
cloviscnc <110499228+cloviscnc@users.noreply.github.com>
2022-08-12 20:48:03 -0300

    Add files via upload

--- 003-projeto/app.js
+++ 003-projeto/app.js
@@ -7,3 +7,9 @@ var novoSalarioInflacao = novoSalario - (inflacao * novoSalario);
 
 console.log (novoSalario);
 console.log (novoSalarioInflacao);
+
+var strnovoSalario = `O novo salario é:\n R$\t ${novoSalario}`;
+var strnovoSalarioInflacao = `O novo salario aplicada inflacao é:\n R$ ${novoSalarioInflacao}`;
+
+console.log (strnovoSalario);
+console.log (strnovoSalarioInflacao);
